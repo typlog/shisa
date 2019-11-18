430ece8eb1ffe0c706213b3ba2b18015c1d00276
Jessie Ji <jessunix@gmail.com>
2019-11-18 18:29:39 +0900

    allow displaying progress with other tags

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,76 +1,88 @@
 const _CONFIG = {
   seekable: true,
   showBuffer: true,
+  progress: true,
 }
 
 export default function progress(el, audio, ctx, options = {}) {
   const name = 'data-progress'
   handleOptions(options)
+  const barPlayed = options.progress ? document.createElement('progress') : document.createElement('div')
+  barPlayed.setAttribute(`${name}-played`, '')
+  el.append(barPlayed)
 
-  let barPlayed = el.querySelector(`progress[${name}-played]`)
-  if (!barPlayed) {
-    barPlayed = document.createElement('progress')
-    barPlayed.setAttribute(`${name}-played`, '')
-    el.append(barPlayed)
+  if (options.progress) {
+    barPlayed.max = 100
+    barPlayed.value = 0
+
+    ctx.on('loadedmetadata', () => {
+      if (ctx.metadataIsFetched && ctx.duration) {
+        barPlayed.value = audio.currentTime / ctx.duration * barPlayed.max
+      }
+    })
+
+    ctx.on('timeupdate', () => {
+      if (ctx.metadataIsFetched && ctx.duration) {
+        barPlayed.value = audio.currentTime / ctx.duration * barPlayed.max
+      }
+    })
+  } else {
+    barPlayed.setAttribute(`${name}-played-value`, 0)
+
+    ctx.on('loadedmetadata', () => {
+      if (ctx.metadataIsFetched && ctx.duration) {
+        barPlayed.setAttribute(`${name}-played-value`, audio.currentTime / ctx.duration * 100)
+      }
+    })
+
+    ctx.on('timeupdate', () => {
+      if (ctx.metadataIsFetched && ctx.duration) {
+        barPlayed.setAttribute(`${name}-played-value`, audio.currentTime / ctx.duration * 100)
+      }
+    })
   }
-  barPlayed.max = 100
-  barPlayed.value = 0
-  ctx.on('loadedmetadata', () => {
-    if (ctx.metadataIsFetched && ctx.duration) {
-      barPlayed.value = audio.currentTime / ctx.duration * barPlayed.max
-    }
-  })
 
-  ctx.on('timeupdate', () => {
-    if (ctx.metadataIsFetched && ctx.duration) {
-      barPlayed.value = audio.currentTime / ctx.duration * barPlayed.max
-    }
-  })
   ctx.on('ended', () => {
     audio.currentTime = 0
   })
 
-
   if (options.showBuffer) {
-    let barBuffered = el.querySelector(`progress[${name}-buffered]`)
-    if (!barBuffered) {
-      barBuffered = document.createElement('progress')
-      barBuffered.setAttribute(`${name}-buffered`, '')
-      el.append(barBuffered)
-    }
-    barBuffered.max = '100'
-    barBuffered.value = 0
+    let barBuffered = options.progress ? document.createElement('progress') : document.createElement('div')
+    barBuffered.setAttribute(`${name}-buffered`, '')
+    el.append(barBuffered)
 
-    ctx.on('progress', () => {
-      if (ctx.metadataIsFetched && ctx.duration) {
-        barBuffered.value = audio.buffered.length ? audio.buffered.end(audio.buffered.length - 1) / ctx.duration * barBuffered.max : 0
-      }
-    })
+    if (options.progress) {
+      barBuffered.max = 100
+      barBuffered.value = 0
+
+      ctx.on('progress', () => {
+        if (ctx.metadataIsFetched && ctx.duration) {
+          barBuffered.value = audio.buffered.length ? audio.buffered.end(audio.buffered.length - 1) / ctx.duration * barBuffered.max : 0
+        }
+      })
+    } else {
+      barBuffered.setAttribute(`${name}-buffered-value`, 0)
+
+      ctx.on('progress', () => {
+        if (ctx.metadataIsFetched && ctx.duration) {
+          const value = audio.buffered.length ? audio.buffered.end(audio.buffered.length - 1) / ctx.duration * 100 : 0
+          barBuffered.setAttribute(`${name}-buffered-value`, value)
+        }
+      })
+    }
   }
 
   if (options.seekable) {
     let initSeek = null
     let dragging = false
-    let slider = el.querySelector(`input[${name}-slider]`)
-    if (!slider) {
-      slider = document.createElement('input')
-      slider.setAttribute(`${name}-slider`, '')
-      el.append(slider)
-    }
+    const slider = document.createElement('input')
+    slider.setAttribute(`${name}-slider`, '')
+    el.append(slider)
     slider.type = 'range'
-    slider.min = '0'
-    slider.max = '100'
+    slider.min = 0
+    slider.max = 100
+    slider.value = 0
 
-    if (slider.getAttribute('value') !== null) {
-      ctx.on('loadedmetadata', () => {
-        if (ctx.metadataIsFetched && ctx.duration) {
-          ctx.seek(barPlayed.value / barPlayed.max * ctx.duration)
-        }
-      })
-    } else {
-      slider.value = 0
-      slider.setAttribute('value', slider.value)
-    }
     slider.addEventListener('input', () => {
       if (!dragging) {
         dragging = true
@@ -106,4 +118,5 @@ export default function progress(el, audio, ctx, options = {}) {
 function handleOptions(options) {
   options.seekable = options.seekable === undefined ? _CONFIG.seekable : options.seekable
   options.showBuffer = options.showBuffer === undefined ? _CONFIG.showBuffer : options.showBuffer
+  options.progress = options.progress === undefined ? _CONFIG.progress : options.progress
 }
